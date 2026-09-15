173298d2e4ff39ee9b2ecb0b76685decd9576615
Piyush Kumar <155344190+piyush886500@users.noreply.github.com>
2026-09-15 11:53:36 +0530

    Time: 11 ms (5.28%) | Memory: 22.4 MB (5.10%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -4,13 +4,24 @@ class Solution:
         l=0
         r=len(numbers)-1
         ans =[0]*2
-        for l in range(len(numbers)):
-            total = target-numbers[l]
-            if total in map:
-                ans[0]=map[total]+1
-                ans[1]=l+1
+        while l<r:
+            total=numbers[l]+numbers[r]
+            if total==target:
+                ans[0]=l+1
+                ans[1]=r+1
                 return ans
+            if total>target:
+                r-=1
             else:
-                map[numbers[l]]=l
+                l+=1
+
+        # for l in range(len(numbers)):
+        #     total = target-numbers[l]
+        #     if total in map:
+        #         ans[0]=map[total]+1
+        #         ans[1]=l+1
+        #         return ans
+        #     else:
+        #         map[numbers[l]]=l
         return ans
 
